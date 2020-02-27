26b2b45d7193b523483902eacad90cebf0c7a9fc
KIM <tpgud33@gmail.com>
2020-02-27 15:02:53 +0900

    Updates

---
+++ precache-manifest.bead4f156697226d4dba08194416a666.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcd2f15838feb3ba72889d80a7ccfe29",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "ba02ae3bbae50da97f96",
+    "url": "/my-app/static/css/main.95a5508f.chunk.css"
+  },
+  {
+    "revision": "4dbf8b82f1ddca54e279",
+    "url": "/my-app/static/js/2.423054aa.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/my-app/static/js/2.423054aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba02ae3bbae50da97f96",
+    "url": "/my-app/static/js/main.121fbd08.chunk.js"
+  },
+  {
+    "revision": "0c10fa8248c25a2f1677",
+    "url": "/my-app/static/js/runtime-main.dd12a99c.js"
+  }
+]);
